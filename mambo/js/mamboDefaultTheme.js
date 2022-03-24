9e261cfdd00dec26bf2a9383e87179f3f7fc2abe
lucas <lucas1vinet@gmail.com>
2022-03-23 22:53:46 -0300

    Function to class component

--- mambo/js/mamboDefaultTheme.js
+++ mambo/js/mamboDefaultTheme.js
@@ -27,6 +27,30 @@ const g_mamboDefaultTheme = {
         contentParent: "mambo-tab-contents",
         parent: "mambo-tab",
         tabs: "mambo-tabs",
+    },
+    "mambo-date-picker": {
+        parent: "date-picker-parent",
+        dropdownWrapper: "date-picker-dropdown-wrapper",
+        input: {
+            inputWrapper: "date-picker-input-wrapper",
+            input: "date-picker-input-input",
+        },
+        dropdown: {
+            parent: "date-picker-dropdown-parent",
+            container: "date-picker-dropdown-container",
+            button: {
+                button: "date-picker-button"
+            }
+        },
+        calendar: {
+            parent: "date-picker-calendar-parent",
+            body: "date-picker-calendar-body",
+            bodyHeader: "date-picker-calendar-body-header",
+            bodyContent: "date-picker-calendar-body-content",
+            headerButtonGroup: {
+                parent: "date-picker-calendar-header"
+            }
+        }
     }
 
 };
