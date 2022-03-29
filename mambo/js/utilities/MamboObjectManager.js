8d4569486bc73db7590c05907701c63ec69aabb1
nfavala <nfavala@hotmail.com>
2022-03-29 08:47:00 -0300

    mambo object manager class refactor V1

--- mambo/js/utilities/MamboObjectManager.js
+++ mambo/js/utilities/MamboObjectManager.js
@@ -17,7 +17,6 @@
  *  Created On : Sat Feb 26 2022
  *  File : MamboObjectManager.js
  *******************************************/
-
 class MamboObjectManager {
     constructor(){
        // Object library
