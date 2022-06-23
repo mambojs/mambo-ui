52a1388de3914263c121dc6c6b23d9b049be2699
Javier Basualdo <javierbasualdocom@gmail.com>
2022-06-23 12:32:53 -0300

    UI refactor function to classes (#24)
    * dev
    
    * test webcomponent class
    
    not sure
    
    * last update

--- src/ui/MamboRating/MamboRating.js
+++ src/ui/MamboRating/MamboRating.js
@@ -19,211 +19,215 @@
  *******************************************/
  import styles from './MamboRating.css';
 
-window.ui.rating = function MamboRating(parentTag, options) {
-    "use strict";
+ui.rating = class MamboRating extends HTMLElement {
+    constructor(initOptions) {
+        super();
 
-    if (!parentTag) {
-        console.error(`Rating: parentEle parameter not passed in.`);
-        return;
-    }
-
-    const self = this;
-    const m_utils = tools.utils;
-
-    // HTML tag variables
-    let m_parentTag;
-    let m_ratingParentTag;
-    let m_ratingEmptyTag;
-    let m_ratingSelectedTag;
-    let m_ratingHoverTag;
-
-    let m_config;
-    let m_value = 0;
-    let m_enable = true;
-
-    // Configure public methods
-    this.destroy = destroyRating;
-    this.enable = enable;
-    this.getParentTag = () => m_ratingParentTag;
-    this.value = value;
-
-    // Config default values
-    configure();
-
-    // Begin setup
-    setup();
-
-    function setup() {
-        m_parentTag = dom.getTag(parentTag);
-
-        if (!m_parentTag) {
-            console.error(`Rating: dom. parent tag ${parentTag} was not found.`);
+        if (!initOptions.parentTag) {
+            console.error(`Rating: parentEle parameter not passed in.`);
             return;
         }
 
-        setOptionValues();
-        installDOM();
-    }
+        const self = this;
+        const m_utils = tools.utils;
 
-    function setOptionValues() {
-        m_value = m_config.value;
-        m_enable = m_config.enable;
-    }
+        // HTML tag variables
+        let m_parentTag;
+        let m_ratingParentTag;
+        let m_ratingEmptyTag;
+        let m_ratingSelectedTag;
+        let m_ratingHoverTag;
 
-    function installDOM() {
-        installTags();
-        finishSetup();
-    }
+        let m_config;
+        let m_value = 0;
+        let m_enable = true;
 
-    function installTags() {
-        m_ratingParentTag = dom.createTag(m_config.tag.rating, { class: m_config.css.parent });
-        dom.append(m_parentTag, m_ratingParentTag);
+        // Configure public methods
+        this.destroy = destroyRating;
+        this.enable = enable;
+        this.getParentTag = () => m_ratingParentTag;
+        this.value = value;
 
-        installLayers();
-        setupEventHandler();
+        // Config default values
+        configure();
 
-        setValue(m_value);
-        setEnable(m_enable);
-    }
+        // Begin setup
+        setup();
 
-    function installLayers() {
-        m_ratingEmptyTag = dom.createTag(m_config.tag.empty, { class: m_config.css.empty });
-        dom.append(m_ratingParentTag, m_ratingEmptyTag);
+        function setup() {
+            m_parentTag = dom.getTag(initOptions.parentTag);
 
-        m_ratingSelectedTag = dom.createTag(m_config.tag.selected, { class: m_config.css.selected });
-        dom.append(m_ratingParentTag, m_ratingSelectedTag);
+            if (!m_parentTag) {
+                console.error(`Rating: dom. parent tag ${initOptions.parentTag} was not found.`);
+                return;
+            }
 
-        m_ratingHoverTag = dom.createTag(m_config.tag.hover, { class: m_config.css.hover });
-        dom.append(m_ratingParentTag, m_ratingHoverTag);
-
-        installStars();
-    }
-
-    function installStars() {
-        for (let i = 0; i < m_config.max; i++) {
-            let emptyStarTag = dom.createTag(m_config.tag.emptyStar, { class: m_config.css.emptyStar });
-            dom.append(m_ratingEmptyTag, emptyStarTag);
-
-            let selectedStarTag = dom.createTag(m_config.tag.selectedStar, { class: m_config.css.selectedStar });
-            dom.append(m_ratingSelectedTag, selectedStarTag);
-
-            let hoverStarTag = dom.createTag(m_config.tag.hoverStar, { class: m_config.css.hoverStar });
-            dom.append(m_ratingHoverTag, hoverStarTag);
+            setOptionValues();
+            installDOM();
         }
-    }
 
-    function setupEventHandler() {
-        m_ratingParentTag.addEventListener("click", selectValue);
-        m_ratingParentTag.addEventListener("mouseenter", setHoverValue);
-        m_ratingParentTag.addEventListener("mousemove", setHoverValue);
-        m_ratingParentTag.addEventListener("mouseleave", hideHoverLayer);
-    }
+        function setOptionValues() {
+            m_value = m_config.value;
+            m_enable = m_config.enable;
+        }
 
-    function selectValue(ev) {
-        if (m_enable) {
-            setValue(getHoverValue(ev));
+        function installDOM() {
+            installTags();
+            finishSetup();
+        }
 
-            if (m_config.fnSelect) {
-                m_config.fnSelect({ rating: self, ev: ev });
+        function installTags() {
+            m_ratingParentTag = dom.createTag(m_config.tag.rating, { class: m_config.css.parent });
+            dom.append(m_parentTag, m_ratingParentTag);
+
+            installLayers();
+            setupEventHandler();
+
+            setValue(m_value);
+            setEnable(m_enable);
+        }
+
+        function installLayers() {
+            m_ratingEmptyTag = dom.createTag(m_config.tag.empty, { class: m_config.css.empty });
+            dom.append(m_ratingParentTag, m_ratingEmptyTag);
+
+            m_ratingSelectedTag = dom.createTag(m_config.tag.selected, { class: m_config.css.selected });
+            dom.append(m_ratingParentTag, m_ratingSelectedTag);
+
+            m_ratingHoverTag = dom.createTag(m_config.tag.hover, { class: m_config.css.hover });
+            dom.append(m_ratingParentTag, m_ratingHoverTag);
+
+            installStars();
+        }
+
+        function installStars() {
+            for (let i = 0; i < m_config.max; i++) {
+                let emptyStarTag = dom.createTag(m_config.tag.emptyStar, { class: m_config.css.emptyStar });
+                dom.append(m_ratingEmptyTag, emptyStarTag);
+
+                let selectedStarTag = dom.createTag(m_config.tag.selectedStar, { class: m_config.css.selectedStar });
+                dom.append(m_ratingSelectedTag, selectedStarTag);
+
+                let hoverStarTag = dom.createTag(m_config.tag.hoverStar, { class: m_config.css.hoverStar });
+                dom.append(m_ratingHoverTag, hoverStarTag);
+            }
+        }
+
+        function setupEventHandler() {
+            m_ratingParentTag.addEventListener("click", selectValue);
+            m_ratingParentTag.addEventListener("mouseenter", setHoverValue);
+            m_ratingParentTag.addEventListener("mousemove", setHoverValue);
+            m_ratingParentTag.addEventListener("mouseleave", hideHoverLayer);
+        }
+
+        function selectValue(ev) {
+            if (m_enable) {
+                setValue(getHoverValue(ev));
+
+                if (m_config.fnSelect) {
+                    m_config.fnSelect({ rating: self, ev: ev });
+                }
+            }
+        }
+
+        function setHoverValue(ev) {
+            if (m_enable) {
+                m_ratingSelectedTag.style.display = "none";
+                m_ratingHoverTag.style.display = "block";
+                m_ratingHoverTag.style.width = getStarWidth() * getHoverValue(ev) + "px";
+            }
+        }
+
+        function hideHoverLayer(ev) {
+            if (m_enable) {
+                m_ratingHoverTag.style.display = "none";
+                m_ratingSelectedTag.style.display = "block";
+            }
+        }
+
+        function getStarWidth() {
+            return m_ratingEmptyTag.clientWidth / m_config.max;
+        }
+
+        function getLeftPosition(ev) {
+            return ev.clientX - m_ratingParentTag.getBoundingClientRect().left;
+        }
+
+        function getHoverValue(ev) {
+            return Math.ceil(getLeftPosition(ev) / getStarWidth());
+        }
+
+        function value(context = {}) {
+            if (typeof context.value === 'undefined') {
+                return m_value;
+            } else {
+                setValue(context.value);
+            }
+        }
+
+        function setValue(value) {
+            m_value = value;
+            m_ratingSelectedTag.style.display = "block";
+            m_ratingHoverTag.style.display = "none";
+            m_ratingSelectedTag.style.width = getStarWidth() * m_value + "px";
+        }
+
+        function enable(context = {}) {
+            if (typeof context.enable === 'undefined') {
+                return m_enable;
+            } else {
+                setEnable(context.enable);
+            }
+        }
+
+        function setEnable(enable) {
+            m_enable = enable;
+            m_enable ? dom.removeClass(m_ratingParentTag, m_config.css.disabled) : dom.addClass(m_ratingParentTag, m_config.css.disabled);
+        }
+
+        function destroyRating() {
+            dom.remove(m_ratingParentTag);
+        }
+
+        function finishSetup() {
+            // Execute complete callback function
+            if (m_config.fnComplete) {
+                m_config.fnComplete({ rating: self });
+            }
+        }
+
+        function configure() {
+            m_config = {
+                css: {
+                    parent: "rating-parent",
+                    empty: "rating-empty",
+                    selected: "rating-selected",
+                    hover: "rating-hover",
+                    emptyStar: "rating-empty-star",
+                    selectedStar: "rating-selected-star",
+                    hoverStar: "rating-hover-star",
+                    disabled: "rating-disabled"
+                },
+                tag: {
+                    rating: "sc-rating",
+                    empty: "rating-empty",
+                    selected: "rating-selected",
+                    hover: "rating-hover",
+                    emptyStar: "rating-empty-star",
+                    selectedStar: "rating-selected-star",
+                    hoverStar: "rating-hover-star"
+                },
+                value: 0,
+                max: 5,
+                enable: true
+            };
+
+            // If options provided, override default config
+            if (initOptions) {
+                m_config = m_utils.extend(true, m_config, initOptions);
             }
         }
     }
-
-    function setHoverValue(ev) {
-        if (m_enable) {
-            m_ratingSelectedTag.style.display = "none";
-            m_ratingHoverTag.style.display = "block";
-            m_ratingHoverTag.style.width = getStarWidth() * getHoverValue(ev) + "px";
-        }
-    }
-
-    function hideHoverLayer(ev) {
-        if (m_enable) {
-            m_ratingHoverTag.style.display = "none";
-            m_ratingSelectedTag.style.display = "block";
-        }
-    }
-
-    function getStarWidth() {
-        return m_ratingEmptyTag.clientWidth / m_config.max;
-    }
-
-    function getLeftPosition(ev) {
-        return ev.clientX - m_ratingParentTag.getBoundingClientRect().left;
-    }
-
-    function getHoverValue(ev) {
-        return Math.ceil(getLeftPosition(ev) / getStarWidth());
-    }
-
-    function value(context = {}) {
-        if (typeof context.value === 'undefined') {
-            return m_value;
-        } else {
-            setValue(context.value);
-        }
-    }
-
-    function setValue(value) {
-        m_value = value;
-        m_ratingSelectedTag.style.display = "block";
-        m_ratingHoverTag.style.display = "none";
-        m_ratingSelectedTag.style.width = getStarWidth() * m_value + "px";
-    }
-
-    function enable(context = {}) {
-        if (typeof context.enable === 'undefined') {
-            return m_enable;
-        } else {
-            setEnable(context.enable);
-        }
-    }
-
-    function setEnable(enable) {
-        m_enable = enable;
-        m_enable ? dom.removeClass(m_ratingParentTag, m_config.css.disabled) : dom.addClass(m_ratingParentTag, m_config.css.disabled);
-    }
-
-    function destroyRating() {
-        dom.remove(m_ratingParentTag);
-    }
-
-    function finishSetup() {
-        // Execute complete callback function
-        if (m_config.fnComplete) {
-            m_config.fnComplete({ rating: self });
-        }
-    }
-
-    function configure() {
-        m_config = {
-            css: {
-                parent: "rating-parent",
-                empty: "rating-empty",
-                selected: "rating-selected",
-                hover: "rating-hover",
-                emptyStar: "rating-empty-star",
-                selectedStar: "rating-selected-star",
-                hoverStar: "rating-hover-star",
-                disabled: "rating-disabled"
-            },
-            tag: {
-                rating: "sc-rating",
-                empty: "rating-empty",
-                selected: "rating-selected",
-                hover: "rating-hover",
-                emptyStar: "rating-empty-star",
-                selectedStar: "rating-selected-star",
-                hoverStar: "rating-hover-star"
-            },
-            value: 0,
-            max: 5,
-            enable: true
-        };
-
-        // If options provided, override default config
-        if (options) {
-            m_config = m_utils.extend(true, m_config, options);
-        }
-    }
 }
+
+customElements.define('mambo-rating', ui.rating);
