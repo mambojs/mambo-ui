3c69fa2c526022d517cda9a1698418e41c3c3e5e
Franco <franco_10fr@hotmail.com>
2022-07-05 10:48:24 -0300

    bug-fixes-grid

--- src/ui/MamboGrid/MamboGrid.js
+++ src/ui/MamboGrid/MamboGrid.js
@@ -318,8 +318,9 @@ ui.grid = class MamboGrid extends HTMLElement {
                     parentTag: context.parentTag
                 });
             };
-    
-            const buttonTag = new ui.button(context.parentTag, buttonConfig);
+            
+            buttonConfig.parentTag=context.parentTag;
+            const buttonTag = new ui.button( buttonConfig);
             addComponentToMap({ column: context.column, colIndex: context.colIndex, component: buttonTag });
         }
     
@@ -368,8 +369,8 @@ ui.grid = class MamboGrid extends HTMLElement {
                     context.column.fnClick(contextClick);
                 }
             };
-    
-            const inputTag = new ui.input(context.parentTag, inputConfig);
+            inputConfig.parentTag=context.parentTag;
+            const inputTag = new ui.input( inputConfig);
             addComponentToMap({ column: context.column, colIndex: context.colIndex, component: inputTag });
         }
     
@@ -440,8 +441,9 @@ ui.grid = class MamboGrid extends HTMLElement {
                     parentTag: context.parentTag
                 });
             };
-    
-            const buttonTag = new ui.button(context.parentTag, buttonConfig);
+            
+            buttonConfig.parentTag=context.parentTag;
+            const buttonTag = new ui.button(buttonConfig);
             addComponentToMap({ column: context.column, colIndex: context.colIndex, component: buttonTag });
         }
     
@@ -469,7 +471,8 @@ ui.grid = class MamboGrid extends HTMLElement {
             config.fnClick = () => {
                 slideoutTag.open();
             };
-            const buttonTag = new ui.button(context.parentTag, config);
+            config.parentTag=context.parentTag;
+            const buttonTag = new ui.button(config);
     
             const slideoutConfig = m_utils.extend(true, {}, config);
             slideoutConfig.fnComplete = config.fnInstallContent;
