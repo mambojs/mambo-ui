aabf50309ea3889b0f1134849545f5524e210bd3
diegonicita <diegonicita@gmail.com>
2024-10-30 19:14:58 -0300

    fix conflicts

--- src/ui/components/Radio/story/radio.js
+++ src/ui/components/Radio/story/radio.js
@@ -1,8 +1,17 @@
 function storyRadio(selectedStory) {
-	const config = {
+	const config1 = {
 		parentTag: selectedStory.parentTag,
 		text: "Radio",
+		position: "right",
 	};
 
-	ui.radio(config);
+	ui.radio(config1);
+
+	const config2 = {
+		parentTag: selectedStory.parentTag,
+		text: "Radio",
+		position: "left",
+	};
+
+	ui.radio(config2);
 }
